764650d2e42dc7a85763916aa355e244a505c025
Matwey Ivanov <matvey.ivanov@medeum.ru>
2023-12-25 12:20:03 +0000

    Add ClearML

--- src/train.py
+++ src/train.py
@@ -1,12 +1,11 @@
 import os
-from typing import Any
+import clearml
 from os import path as osp
 
 import click
 
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks import LearningRateMonitor, RichProgressBar
-from pytorch_lightning import seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor, RichProgressBar, ModelCheckpoint
 from src.config import Config
 from src.constants import EXPERIMENTS_PATH
 from src.datamodule import OCRDM
@@ -16,8 +15,8 @@ from src.lightning_module import OCRModule
 @click.command()
 @click.argument('config_path')
 def main(config_path: str):
-    seed_everything(42, workers=True)
     config = Config.from_yaml(config_path)
+    pl.seed_everything(config.rand_seed, workers=True)
     train(config)
 
 
@@ -28,11 +27,28 @@ def train(config: Config):
     experiment_save_path = osp.join(EXPERIMENTS_PATH, config.experiment_name)
     os.makedirs(experiment_save_path, exist_ok=True)
 
+    task = clearml.Task.init(
+        project_name=config.project_name,
+        task_name=config.experiment_name,
+        auto_connect_frameworks=True,
+    )
+
+    task.connect(config.model_dump())
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=experiment_save_path,
+        monitor=config.monitor_metric,
+        mode=config.monitor_mode,
+        save_top_k=1,
+        filename=f'epoch_{{epoch:02d}}-{{{config.monitor_metric}:.3f}}',
+    )
+
     trainer = pl.Trainer(
         max_epochs=config.n_epochs,
         accelerator=config.accelerator,
         devices=[config.device],
         callbacks=[
+            checkpoint_callback,
             LearningRateMonitor(logging_interval='epoch'),
             RichProgressBar(),
         ],
@@ -40,6 +56,7 @@ def train(config: Config):
     )
 
     trainer.fit(model=model, datamodule=datamodule)
+    trainer.validate(ckpt_path=checkpoint_callback.best_model_path, datamodule=datamodule)
 
 
 if __name__ == '__main__':
